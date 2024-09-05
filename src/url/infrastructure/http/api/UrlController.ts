6cbf0dcf7a3685f0fd64af67bd81c074e4f00551
Benjamin Rae <benjaminrae93@gmail.com>
2024-09-05 02:13:50 +0200

    fix: HttpStatus import

--- src/url/infrastructure/http/api/UrlController.ts
+++ src/url/infrastructure/http/api/UrlController.ts
@@ -4,7 +4,7 @@ import {type UrlShortener} from "../../../domain/UrlShortener";
 import {type Configuration} from "../../../../config/Configuration";
 import {ShortenedUrlResponse} from "./ShortenedUrlResponse.js";
 import ServerError from "../../../../shared/infrastructure/http/server/middlewares/errors/ServerError/ServerError.js";
-import {HttpStatus} from "../../../../shared/infrastructure/http/HttpStatus";
+import {HttpStatus} from "../../../../shared/infrastructure/http/HttpStatus.js";
 
 export class UrlController {
   constructor(private readonly shortenUrlService: UrlShortener, private readonly configuration: Configuration) {
